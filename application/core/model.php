d265373de25d73f5e1d9af03cff3157c3e8ce442
Kozma Oliver <kozma.oliver@wawona.hu>
2021-05-17 15:58:23 +0200

    model hotfix

--- application/core/model.php
+++ application/core/model.php
@@ -2,13 +2,15 @@
 
 class Model
 {
-    /**
-     * @param object $db A PDO database connection
-     */
-    function __construct($db)
+
+    public static function connect()
     {
         try {
-            $this->db = $db;
+            // set the optional options of the PDO connection. fetch mode to objects
+            $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING);
+
+            // generate a database connection, using the PDO connector
+            return new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET, DB_USER, DB_PASS, $options);
         } catch (PDOException $e) {
             exit('Database connection could not be established.');
         }
